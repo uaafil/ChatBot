01378be5c3592ddecd5282ea99a1c267f4f1702a
uaafil <102312487+uaafil@users.noreply.github.com>
2023-09-05 11:30:00 +0530

    Add files via upload

---
+++ ChatBot.py
@@ -0,0 +1,34 @@
+import openai
+
+# Set your OpenAI API key
+openai.api_key = "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"
+
+# Initial message to start the conversation
+conversation = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Tell me a joke."},
+]
+
+# Continuously interact with the chatbot
+print("Chatbot: Hi there! I'm here to help. What can I assist you with?")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        print("Chatbot: Goodbye!")
+        break
+
+    # Append user input to the conversation
+    conversation.append({"role": "user", "content": user_input})
+
+    # Use GPT-3 to generate a response
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # Use the latest model name
+        messages=conversation
+    )
+
+    # Extract and print the chatbot's response
+    chatbot_response = response['choices'][0]['message']['content']
+    print("Chatbot:", chatbot_response)
+
+    # Append chatbot response to the conversation
+    conversation.append({"role": "assistant", "content": chatbot_response})
